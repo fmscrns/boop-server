029f18d4c2ac19949a54648ccd4abb497a7b9bcd
fmscrns <fmscrns@gmail.com>
2021-01-15 22:45:47 +0800

    fix config vars

--- app/main/config.py
+++ app/main/config.py
@@ -1,5 +1,11 @@
 import os
 
+# uncomment the line below for postgres database url from environment variable
+# postgres_local_base = os.environ['BOOPDEV_DB_URL']
+postgres_remote_base = os.environ['BOOPPROD_DB_URL']
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
     DEBUG = False
@@ -7,7 +13,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
-    SQLALCHEMY_DATABASE_URI = os.environ['BOOPDEV_DB_URL']
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -16,7 +22,7 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     DEBUG = False
     # uncomment the line below to use postgres
-    SQLALCHEMY_DATABASE_URI = "postgres://ofxsqguhwuapcz:967213862cd923b9ace9dca586d4e6a9eacd0b5dda5903d145b537f743f86e61@ec2-52-204-113-104.compute-1.amazonaws.com:5432/dssj73099d507"
+    SQLALCHEMY_DATABASE_URI = postgres_remote_base
 
 
 config_by_name = dict(
