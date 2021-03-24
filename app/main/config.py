50aa7ca24fea96a4fecb423f1bb353a230be2918
fmscrns <fmscrns@gmail.com>
2021-03-24 12:17:14 +0800

    fix config for prod

--- app/main/config.py
+++ app/main/config.py
@@ -1,7 +1,7 @@
 import os
 
 # uncomment the line below for postgres database url from environment variable
-postgres_local_base = os.environ['BOOPDEV_DB_URL']
+# postgres_local_base = os.environ['BOOPDEV_DB_URL']
 postgres_remote_base = os.environ['BOOPPROD_DB_URL']
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -13,7 +13,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
-    SQLALCHEMY_DATABASE_URI = postgres_local_base
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
