50811fbdef671b6112e31969b1708400c1c4e419
fmscrns <fmscrns@gmail.com>
2021-07-16 16:06:34 +0800

    fixed config

--- app/main/config.py
+++ app/main/config.py
@@ -1,9 +1,9 @@
 import os
 
-def local_base():
-    return os.environ['BOOPDEV_DB_URL']
+# # uncomment the line below for postgres database url from environment variable
+# postgres_local_base = os.environ['BOOPDEV_DB_URL']
 
-# postgres_remote_base = os.environ['BOOPPROD_DB_URL']
+postgres_remote_base = os.environ['BOOPPROD_DB_URL']
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,7 +14,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
-    SQLALCHEMY_DATABASE_URI = local_base()
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIN_DOMAIN = "http://127.0.0.1:8080"
@@ -25,7 +25,7 @@ class ProductionConfig(Config):
     MAIN_DOMAIN = "https://boop-proj-client.herokuapp.com"
     PER_PAGE_PAGINATION = 7
     # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_remote_base
+    SQLALCHEMY_DATABASE_URI = postgres_remote_base
 
 
 config_by_name = dict(
