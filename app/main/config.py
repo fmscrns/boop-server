a9567c9aabbfcc17a5d087c6c02eba65f50f5a7a
fmscrns <fmscrns@gmail.com>
2021-07-16 23:09:25 +0800

    bug fix: no notif on owned pet being tagged on a post

--- app/main/config.py
+++ app/main/config.py
@@ -1,7 +1,7 @@
 import os
 
 # # uncomment the line below for postgres database url from environment variable
-# postgres_local_base = os.environ['BOOPDEV_DB_URL']
+postgres_local_base = os.environ['BOOPDEV_DB_URL']
 
 postgres_remote_base = os.environ['BOOPPROD_DB_URL']
 
@@ -14,7 +14,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIN_DOMAIN = "http://127.0.0.1:8080"
