7693f4d58d763f4f948bcbcc52e72620907fff89
Francis Mascariñas <fmscrns@gmail.com>
2021-12-08 01:34:31 +0800

    Update config.py

--- app/main/config.py
+++ app/main/config.py
@@ -9,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
     DEBUG = False
-
+    PER_PAGE_PAGINATION = 4
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
@@ -18,13 +18,11 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIN_DOMAIN = "http://127.0.0.1:8080"
-    PER_PAGE_PAGINATION = 10
 
 class ProductionConfig(Config):
     ENV = "production"
     DEBUG = False
     MAIN_DOMAIN = "https://boop-proj-client.herokuapp.com"
-    PER_PAGE_PAGINATION = 7
     # uncomment the line below to use postgres
     SQLALCHEMY_DATABASE_URI = postgres_remote_base
 
